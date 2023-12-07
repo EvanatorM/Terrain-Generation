0c37301762ae20e97e496d06b5b9cd56a749f55e
WSAL Evan <evan@millers6.com>
2023-12-06 22:26:29 -0500

    Finished plane generation and shader

--- Assets/ChunkDemo.cs
+++ Assets/ChunkDemo.cs
@@ -25,11 +25,14 @@ public class ChunkDemo : MonoBehaviour
 
         Vector3[] vertices = new Vector3[resolution * resolution];
         Vector3[] normals = new Vector3[resolution * resolution];
+
+        float spacing = size / (float)resolution;
+        
         for (int x = 0; x < resolution; x++)
         {
             for (int z = 0; z < resolution; z++)
             {
-                vertices[x * resolution + z] = new Vector3(x, Random.Range(-1f, 1f), z);
+                vertices[x * resolution + z] = new Vector3(x * spacing, Random.Range(-1f, 1f), z * spacing);
                 normals[x * resolution + z] = Vector3.up;
             }
         }
